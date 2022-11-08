00c54e2a560ed1d5b6039d6ea271f72429ae9214
jgoler <jack.goler@gmail.com>
2022-11-08 15:54:55 -0800

    Started working on education section

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,10 +1,38 @@
 import '../App.css';
+import MenloLogo from '../assets/Menlo.png';
+import CourseraStanfordLogo from '../assets/CourseraStanford.png';
 
 function Education() {
   return (
     <div className='EducationContainer'>
       <div className='Education'>
         <div className='EducationTitle'>Education</div>
+        <div className='EducationPositions'>
+          <div className='Menlo'>
+            <a href='https://www.menloschool.org/' target='_blank'>
+              <img className='MenloLogo' src={MenloLogo} alt='Menlo School' />
+              <div className='EducationalInstituteTitle'>Menlo School</div>
+            </a>
+            <div className='EducationalInstituteTimeline'>
+              August 2019 - June 2023
+            </div>
+            <div className='EducationalInsituteQuickStats'>GPA: 4.25</div>
+            <div className='MenloCoursework'>Classes Taken</div>
+          </div>
+          <div className='CourseraStanford'>
+            <img
+              className='CourseraStanfordLogo'
+              src={CourseraStanfordLogo}
+              alt='Stanford Coursera logo'
+            />
+            <div className='EducationalInstituteTitle'>Menlo School</div>
+            <div className='EducationalInstituteTimeline'>
+              August 2019 - June 2023
+            </div>
+            <div className='EducationalInsituteQuickStats'>GPA: 4.25</div>
+            <div className='MenloCoursework'>Classes Taken</div>
+          </div>
+        </div>
       </div>
     </div>
   );
