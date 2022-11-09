b3177a481ca15dbe83a75e27eab6ec3619456513
jgoler <jack.goler@gmail.com>
2022-11-08 18:14:07 -0800

    Fixed the education section to make it dynamic

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,43 +1,68 @@
 import '../App.css';
 import MenloLogo from '../assets/Menlo.png';
 import CourseraStanfordLogo from '../assets/CourseraStanford.png';
+import UdemyLogo from '../assets/UdemyLogo.png';
 
 function Education() {
   return (
     <div className='EducationContainer'>
       <div className='Education'>
         <div className='EducationTitle'>Education</div>
-        <div className='EducationPositions'>
-          <div className='Menlo'>
-            <a href='https://www.menloschool.org/' target='_blank'>
-              <img className='MenloLogo' src={MenloLogo} alt='Menlo School' />
-              <div className='EducationalInstituteTitle'>Menlo School</div>
-            </a>
-            <div className='EducationalInstituteTimeline'>
-              August 2019 - June 2023
+        <div className='RowContainer'>
+          <div className='Row1'>
+            <div className='InstitutionContainer'>
+              <div className='LogoContainer'>
+                <img className='Logo' src={MenloLogo} alt='Menlo School Logo' />
+              </div>
+              <div className='InstitutionTitle'>Menlo School</div>
+              <div className='InstitutionTimeline'>August 2019 - June 2023</div>
+              <div className='InstitutionQuickStats'>
+                GPA: 4.25 / 4.00, SAT: 1570 (800 math, 770 EBRW)
+              </div>
+              <div className='EducationDescription'>
+                12th grade coursework includes AP Calculus BC, Advanced Topics
+                in Math (H): Number Theory, Advanced Topics in Physics (H):
+                Electromagnetism, Relativity and Quantum Mechanics, and
+                Independent Study in Machine Learning
+              </div>
             </div>
-            <div className='EducationalInsituteQuickStats'>
-              GPA: 4.25, SAT: 1570 (800 math, 770 EBRW)
+            <div className='InstitutionContainer'>
+              <div className='LogoContainer'>
+                <img
+                  className='Logo'
+                  src={CourseraStanfordLogo}
+                  alt='Coursera Stanford Logo'
+                />
+              </div>
+              <div className='InstitutionTitle'>
+                Stanford Online Machine Learning Specialization
+              </div>
+              <div className='InstitutionTimeline'>2022</div>
+              <div className='InstitutionQuickStats'>Final Grade: 100%</div>
+              <br />
+              <div className='EducationDescription'>
+                Topics covered include supervised machine learning, regression
+                and classification, neural networks, recommender systems, tree
+                ensembles, reinforcement learning, and advanced learning
+                algorithms.
+              </div>
             </div>
-            <div className='MenloCoursework'>
-              12th grade coursework includes Advanced Topics in Math (H): Number
-              Theory, Advanced Topics in Physics (H): Electromagnetism,
-              Relativity, and Quantum Mechanics, AP Calculus BC, and Independent
-              Study in Machine Learning
+            <div className='InstitutionContainer'>
+              <div className='LogoContainer'>
+                <img className='Logo' src={UdemyLogo} alt='Udemy Logo' />
+              </div>
+              <div className='InstitutionTitle'>Udemy</div>
+              <div className='InstitutionTimeline'>2017 - Present</div>
+              <div className='InstitutionQuickStats'>
+                100% Completion on 12+ Courses
+              </div>
+              <br />
+              <div className='EducationDescription'>
+                Courses include full stack engineering (MERN stack), Machine
+                Learning and Tensorflow, React, React Native, Python,
+                JavaScript, Java, and WebSockets.
+              </div>
             </div>
-          </div>
-          <div className='CourseraStanford'>
-            <img
-              className='CourseraStanfordLogo'
-              src={CourseraStanfordLogo}
-              alt='Stanford Coursera logo'
-            />
-            <div className='EducationalInstituteTitle'>Menlo School</div>
-            <div className='EducationalInstituteTimeline'>
-              August 2019 - June 2023
-            </div>
-            <div className='EducationalInsituteQuickStats'>GPA: 4.25</div>
-            <div className='MenloCoursework'>Classes Taken</div>
           </div>
         </div>
       </div>
