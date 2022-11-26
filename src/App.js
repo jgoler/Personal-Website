eb9b5b55e77cf72dae61e17b2419ff8877969b04
jgoler <jack.goler@gmail.com>
2022-11-26 01:57:55 -0800

    working on menu bar

--- src/App.js
+++ src/App.js
@@ -6,23 +6,56 @@ import Education from './components/Education';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
 import { MdMenu } from 'react-icons/md';
+import { AiOutlineClose } from 'react-icons/ai';
+import { useRef, useState } from 'react';
 
 function App() {
+  const menuOpenHandler = () => {
+    setMenuStatus(!menuStatus);
+  };
+  //const AboutMeRef = useRef();
+  const [menuStatus, setMenuStatus] = useState(false);
   return (
     <div className='App'>
-      <div className='SidebarContainer'>
-        <div className='MenuContainer'>
-          <MdMenu color='white' size='45' />
+      {menuStatus ? (
+        <div className='SidebarContainer' onClick={menuOpenHandler}>
+          <div className='MenuContainer'>
+            <MdMenu color='white' size='45' />
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className='SidebarContainer' onClick={menuOpenHandler}>
+          <div className='DropDown'>
+            <div className='DropDownListHolder'>
+              <div className='X'>
+                <AiOutlineClose color='white' size='45' />
+              </div>
+              <div className='DropDownItem'>Test1</div>
+              <div className='DropDownItem'>Test2</div>
+              <div className='DropDownItem'>Test3</div>
+              <div className='DropDownItem'>Test4</div>
+              <div className='DropDownItem'>Test5</div>
+            </div>
+          </div>
+        </div>
+      )}
       <Welcome />
       <AboutMe />
       <WorkAndResearch />
       <Education />
       <Projects />
-      <Footer />
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 }
 
 export default App;
+
+/*
+<button onClick={() => scrollToRef.current.scrollIntoView()}>
+        {' '}
+        Test
+      </button>
+*/
