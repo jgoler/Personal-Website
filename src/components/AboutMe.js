b9a9117f77305f978497e63f73f02c582c07e2da
jgoler <jack.goler@gmail.com>
2022-11-30 16:57:42 -0800

    Done with contact

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,21 +5,20 @@ function AboutMe() {
     <div className='AboutMe'>
       <div className='AboutMeTitle'>About Me</div>
       <div className='ParagraphContainer'>
-        Hi, nice to e-meet you! I'm Jack, a senior at Menlo School from Menlo
-        Park, California. I'm an avid coder and mathematician. I enjoy playing
-        games ranging from Poker and Catan to pick up basketball and fast-paced
-        ping pong. You can usually find me golfing (albeit poorly), solving
-        Project Euler problems, brainstorming app ideas, puzzling over
-        competition-based math problems, and coding random projects. Currently,
-        my interests in Computer Science include machine learning and its
-        medical applications, as well as cybersecurity and ethical hacking. My
-        garage is my creative space. I converted it into a maker space to build
-        my projects. While I'm just working on personal projects, it's cool to
-        work in a space that reminds me of the HP, Apple, Google, and other
-        Silicon Valley tech legends’ origin stories. Speaking of tech legends,
-        much like Steve Jobs, I have a limited wardrobe of eight nearly
-        identical outfits, except I wear shorts and t-shirts instead of
-        turtlenecks.
+        Hi! I'm Jack, a senior at Menlo School from Menlo Park, California. I'm
+        an avid coder and mathematician. I enjoy playing games ranging from
+        Poker and Catan to pick up basketball and fast-paced ping pong. You can
+        usually find me golfing (albeit poorly), solving Project Euler problems,
+        brainstorming app ideas, puzzling over competition-based math problems,
+        and coding random projects. Currently, my interests in Computer Science
+        include machine learning and its medical applications, as well as
+        cybersecurity and ethical hacking. My garage is my creative space. I
+        converted it into a maker space to build my projects. While I'm just
+        working on personal projects, it's cool to work in a space that reminds
+        me of the HP, Apple, Google, and other Silicon Valley tech legends’
+        origin stories. Speaking of tech legends, much like Steve Jobs, I have a
+        limited wardrobe of eight nearly identical outfits, except I wear shorts
+        and t-shirts instead of turtlenecks.
       </div>
     </div>
   );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,34 @@
 import '../App.css';
+import { FiMail } from 'react-icons/fi';
+import { AiOutlineGithub } from 'react-icons/ai';
+import { HiDocumentText } from 'react-icons/hi';
 
 function Contact() {
   return (
     <div className='ContactContainer'>
       <div className='Contact'>
         <div className='ContactTitle'>Contact</div>
+        <div className='Contacts'>
+          <div className='SingleContact'>
+            <FiMail color='#122a43' size='30' />
+            <div className='DivForSpace'>jack.goler@gmail.com</div>
+          </div>
+          <div className='SingleContact'>
+            <a href='https://github.com/jgoler' target='_blank'>
+              <AiOutlineGithub color='#122a43' size='30' />
+              <div className='DivForSpace'>JGoler</div>
+            </a>
+          </div>
+          <div className='SingleContact'>
+            <a
+              href='https://docs.google.com/document/d/13vT6TeN7gw4cIxNgHuye8ipOpYXEfY5F0ALDgeRPfSA'
+              target='_blank'
+            >
+              <HiDocumentText color='#122a43' size='35' />
+              <div className='DivForSpace'>Resume</div>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
